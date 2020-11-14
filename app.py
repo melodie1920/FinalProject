3d64ea33ec4094494bb9463282f4aadecf0941f8
mathluva <mathluvacoding@gmail.com>
2020-11-14 13:54:46 -0500

    requirements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import joblib
 from flask import Flask, render_template, redirect, Response, jsonify
-from bson import json_util
-import json
+# from bson import json_util
+# import json
 import sys, getopt, pprint
 from flask_cors import CORS, cross_origin
 import pandas as pd
@@ -18,6 +18,10 @@ CORS(app, resources={
 })
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['CORS_ORIGINS'] = '*'
+
+@app.route("/", methods=['GET',"post"])
+def mainroute():
+    return app.send_static_file("index.html")
 
 @app.route("/predict/<int:gender_arg>/<int:car_arg>/<int:home_arg>/<int:education_arg>/<int:familystatus_arg>/<int:housingtype_arg>/<int:email_arg>/<int:incometype_arg>/<int:children_arg>/<int:income_arg>/<int:family_arg>/<int:employment_arg>/<int:age_arg>", methods=["GET"])
 def predict(gender_arg,car_arg,home_arg,education_arg,familystatus_arg,housingtype_arg,email_arg,incometype_arg,children_arg,income_arg,family_arg,employment_arg,age_arg):
